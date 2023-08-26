e4e179ed0223bf79aba19e20f7e64191cb9682eb
Diddi karthik ganesh <142075105+karthikganesh18@users.noreply.github.com>
2023-08-26 17:15:13 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+function change(){
+    let c1= document.getElementById('c1id')
+    c1.style.display='none'
+    let c2= document.getElementById('c2id')
+    c2.style.display='inline-block'
+
+}
+function back(){
+    let c1= document.getElementById('c1id')
+    c1.style.display='inline-block'
+    let c2= document.getElementById('c2id')
+    c2.style.display='none'
+}
+function change1(){
+    let c1= document.getElementById('c1id')
+    c1.style.display='none'
+    let c2= document.getElementById('ch1')
+    c2.style.display='inline-block'
+}
+function back1(){
+    let c1= document.getElementById('c1id')
+    c1.style.display='inline-block'
+    let c2= document.getElementById('ch1')
+    c2.style.display='none'
+}
+function change2(){
+    let c1= document.getElementById('c1id')
+    c1.style.display='none'
+    let c2= document.getElementById('dr1')
+    c2.style.display='inline-block'
+}
+function back2(){
+    let c1= document.getElementById('c1id')
+    c1.style.display='inline-block'
+    let c2= document.getElementById('dr1')
+    c2.style.display='none'
+}
+function change3(){
+    let c1= document.getElementById('c1id')
+    c1.style.display='none'
+    let c2= document.getElementById('re1')
+    c2.style.display='inline-block'
+}
+function back3(){
+    let c1= document.getElementById('c1id')
+    c1.style.display='inline-block'
+    let c2= document.getElementById('re1')
+    c2.style.display='none'
+}
