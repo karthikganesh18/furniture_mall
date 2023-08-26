631748857cbc05437125d6ebf2de98a6c10e7669
Diddi karthik ganesh <142075105+karthikganesh18@users.noreply.github.com>
2023-08-26 19:58:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,3 +47,17 @@ function back3(){
     let c2= document.getElementById('re1')
     c2.style.display='none'
 }
+function karthik(){
+    
+    let c2= document.getElementById('c2id')
+    c2.style.display='none'
+
+    let c3= document.getElementById('gan')
+    c3.style.display='inline-block'
+}
+function backs(){
+    let c1= document.getElementById('gan')
+    c1.style.display='none'
+    let c2= document.getElementById('c2id')
+    c2.style.display='inline-block'
+}
